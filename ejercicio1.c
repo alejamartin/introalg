fed6ee48dda7da21e9b6132d45e42f16da9e8139
Alejandra Martínez <alejandramartinezvazquez05@gmail.com>
2024-07-15 17:31:00 -0600

    primer ejercicio

---
+++ ejercicio1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Inicio del programa
+    int valor1 = 10;
+    int valor2 = 5;
+    printf("La base del recatangulo es: %d la altura del rectangulo es: %d \n", valor1,valor2);
+    printf("El area del rectangulo es: %d \n", valor1 * valor2);
+    printf("El perimetro del rectangulo es: %d \n", 2*(valor1+valor2));
+
+
+
+    return 0; // Fin del programa
+}
